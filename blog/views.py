107004a138c37c59f22d16fa2430b4e6719d1645
pirosiki <iloveowl0908@outlook.jp>
2020-09-02 19:50:22 +0900

    削除ボタンを追加しました

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts=posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
 
 def post_detail(request,pk):
@@ -29,12 +29,16 @@ def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return redirect('post_detail', pk=post.pk)
+        if 'save' in request.POST:
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            Post.objects.get(pk=pk).delete()
+            return redirect('post_list')
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
